b5f087bf393e83c5c151e0dacb87882d89da0334
peterpirog <peterpirogtf@gmail.com>
2020-12-25 16:40:23 +0100

    initial commit

--- run_main_in_docker.py
+++ run_main_in_docker.py
@@ -8,22 +8,22 @@ os.environ["WANDB_API_KEY"] = '1ed2c67731159621d3caad4917cee0c5e21b3c2f'
 #os.environ["WANDB_MODE"] = "dryrun"
 
 env='BipedalWalker-v3'  #Name of the environment (default: Pendulum-v0)
-info='Experiment-dl1'     #Name of the Experiment (default: Experiment-1)
-frames='40000'          #Number of training frames (default: 40000)
-mem='100000'            #Replay buffer size (default: 100000)
+info='Experiment-base'   #Name of the Experiment (default: Experiment-1)
+frames='5000000'        #Number of training frames (default: 40000)
+mem='1000000'            #Replay buffer size (default: 100000)
 batch_size='128'        #Batch size (default: 128)
 layer_size='256'        #Neural Network layer size (default: 256)
 gamma='0.99'            #Discount factor gamma (default: 0.99)
-tau='1e-3'              #Soft update factor tau (default: 1e-3)
-learning_rate ='1e-3'   #Learning rate (default: 1e-3)
+tau='0.0025'              #Soft update factor tau (default: 1e-3)
+learning_rate ='5e-4'   #Learning rate (default: 1e-3)
 update_every ='1'       #update the network every x step (default: 1)
 n_updates='1'           #update the network for x steps (default: 1)
 seed='0'                #random seed (default: 0)
-choices='0'             #choices=[0,1] : Use prioritized experience replay (default: 0)
-nstep_bootstrapping='1' #nstep_bootstrapping (default: 1)
+choices='1'             #choices=[0,1] : Use prioritized experience replay (default: 0)
+nstep_bootstrapping='2' #nstep_bootstrapping (default: 1)
 d2rl='1'                #Using Deep Dense Network if set to 1 (default: 0)
 eval_every='1000'       #Doing an evaluation of the current policy every X frames (default: 1000)
-eval_runs='3'           #Number of evaluation runs - performance is averaged over all runs (default: 3)
+eval_runs='100'         #Number of evaluation runs - performance is averaged over all runs (default: 3)
 
 cmd=f'python naf2.py ' \
     f'-env {env} ' \
